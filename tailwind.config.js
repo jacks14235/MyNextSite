cf38846083657cff912cfb567b2d7e509cc54c14
Jack Stanley <jack@stanley5.com>
2021-05-17 18:15:34 -0400

    Thats it mister you lost your purge privileges

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
-  ],
+  // purge: [
+  //   './pages/**/*.{js,ts,jsx,tsx}',
+  //   './components/**/*.{js,ts,jsx,tsx}'
+  // ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
